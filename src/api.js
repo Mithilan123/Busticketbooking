dc301cbc05d35a3881431ae9dd063c50c55a9c71
Mithilan <mithilan47@gmail.com>
2025-12-17 14:38:23 +0530

    enhanced site

--- src/api.js
+++ src/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ticketbookingbackend.vercel.app/",
+  baseURL: "http://13.51.64.0:5000/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
 export default API;
+
